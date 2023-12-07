42ade7dfba25298353f9a4254b914ca80017826a
Mark Kibara <markkibara2014@gmail.com>
2023-12-07 23:07:48 +0300

    Add dockefiles

---
+++ carrental/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# This script checks if the container is started for the first time.
+
+CONTAINER_FIRST_STARTUP="CONTAINER_FIRST_STARTUP"
+if [ ! -e /$CONTAINER_FIRST_STARTUP ]; then
+    touch /$CONTAINER_FIRST_STARTUP
+    # place your script that you only want to run on first startup.
+    npx prisma generate && npx nx build api && cd /app/dist/apps/api/
+else
+    # script that should run the rest of the times (instances where you 
+    # stop/restart containers).
+    cd /app/dist/apps/api/ 
+fi
+
+pm2 start main.js
+pm2 logs
