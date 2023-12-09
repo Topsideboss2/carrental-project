a7fc5aa1913fb46197cabd281d4e92d4b5c93bd7
Mark Kibara <markkibara2014@gmail.com>
2023-12-09 10:25:31 +0300

    Add prisma migrate command to entrypoint.sh

--- carrental/entrypoint.sh
+++ carrental/entrypoint.sh
@@ -5,7 +5,7 @@ CONTAINER_FIRST_STARTUP="CONTAINER_FIRST_STARTUP"
 if [ ! -e /$CONTAINER_FIRST_STARTUP ]; then
     touch /$CONTAINER_FIRST_STARTUP
     # place your script that you only want to run on first startup.
-    npx prisma generate && npx nx build api && cd /app/dist/apps/api/
+    npx prisma migrate dev && npx prisma generate && npx nx build api && cd /app/dist/apps/api/
 else
     # script that should run the rest of the times (instances where you 
     # stop/restart containers).
